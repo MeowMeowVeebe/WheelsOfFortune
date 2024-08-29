6dbe11cfbba530f95611f51991e799faaedb4b21
MeowMeowVeebe <156172901+MeowMeowVeebe@users.noreply.github.com>
2024-08-29 16:23:10 +0700

    Add files via upload

---
+++ ConstOfTruthAndDares.js
@@ -0,0 +1,56 @@
+const Truth = [
+"Siapa cewe yang kamu suka",
+"Siapa teman baik kamu",
+"Di masa depan, Kamu mau jadi apa",
+"Orang yang kamu benci siapa?",
+"Games apa yang di masa kecil kamu?",
+"Nama kamu siapa?",
+"Apakah kamu gay?",
+"Nama channel kamu apa?",
+"Pelajaran yang kamu benci apa?",
+"Orang yang kamu takuti siapa?",
+"Ideologi kamu apa?",
+"Teman terpintar dikelas kamu siapa?",
+"Teman terbodoh di kelas kamu siapa?",
+"Teman yang gak guna siapa?",
+"Apakah kamu pernah liat video porno?",
+"Jodoh di masa depang apa?",
+"Cewe kamu siapa?",
+"Orang yang kamu benci siapoa",
+"Apakah kamu rajin sholat?",
+"Nama kamu siapa?",
+"Apakah kamu orang cupu",
+"Kekasih masa depan kamu siapa?",
+
+
+
+
+
+
+
+
+
+];
+
+
+const Dare = [
+    "Kamu harus subscribe channel ryukunyuk",
+    "Kamu harus promosi channel ryukunyuk di WA keluarga dan di sosmed kamu",
+    "Kamu harus nonton salah satu video dari ryukunyuk",
+    "Kamu harus join discord server (Ryukunyuk)",
+    "Kamu harus berteman dengan ray alan",
+    "Kamu harus peluk guru yang bernama daud",
+    "Kamu harus peluk guru yang bernama irfan",
+    "Kamu harus bilang ke guru yang bernama daud i love you",
+    "Kamu harus bilang ke guru yang bernama irfan i love you",
+    "Kamu harus menonton happy tree freinds",
+    "Kamu harus foto orang dan bikin memenya",
+    "Kamu harus nonton cringe video tiktok",
+    "Kamu harus main roblox",
+    "Kamu harus beli PS1",
+    "Kamu harus memberi saran ke orang lain"
+
+     
+
+
+];
